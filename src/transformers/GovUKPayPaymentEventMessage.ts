f6e19f9c8f4bc5e982eefa34bab839d268e53212
Steven <steven.fountain@digital.cabinet-office.gov.uk>
2020-04-22 16:16:49 +0100

    Add message transformer for Pay payment events
    Flat message objects from the S3 stream will be formatted into GOV.UK
    Pay payment events that can be consumed by Ledger. This is the first
    functionality that will apply custom transforms on top the upstream
    fork.

---
+++ src/transformers/GovUKPayPaymentEventMessage.ts
@@ -0,0 +1,55 @@
+/* eslint-disable @typescript-eslint/camelcase */
+import crypto from 'crypto'
+import SQS = require("aws-sdk/clients/sqs")
+import Transformer from './Transformer'
+import { Message } from './../providers/Provider'
+
+interface PaymentEventMessage {
+	event_data: { [key: string]: string };
+	resource_type?: string;
+	resource_external_id?: string;
+	event_date?: string;
+	event_type?: string;
+	parent_resource_external_id?: string;
+	[key: string]: string | { [key: string]: string };
+}
+
+// we can gaurantee the existence of required fields as anything with permissions
+// to this resource will be validating data entry
+function formatPaymentEventMessage(message: Message): PaymentEventMessage {
+	const reservedKeys = [
+		{ key: 'transaction_id', target: 'resource_external_id' },
+		{ key: 'parent_transaction_id', target: 'parent_resource_external_id' },
+		{ key: 'resource_type', target: 'resource_type' },
+		{ key: 'event_date', target: 'event_date' },
+		{ key: 'event_type', target: 'event_type' }
+	]
+	const formatted: PaymentEventMessage = { event_data: {} }
+
+	// initially extract the reserved properties
+	for (const reserved of reservedKeys) {
+		const reservedEntry = message[reserved.key]
+		if (reservedEntry) {
+			formatted[reserved.target] = reservedEntry
+			delete message[reserved.key]
+		}
+	}
+
+	// any remaining properties will override attributes of the transaction itself
+	// put these in `event_data`
+	for (const paymentEventMessageKey in message) {
+		formatted.event_data[paymentEventMessageKey] = message[paymentEventMessageKey]
+	}
+	return formatted
+}
+
+export default class GovUkPayPaymentEventMessage implements Transformer {
+	transform(message: Message): SQS.SendMessageBatchRequestEntry {
+		return {
+			Id: crypto.randomBytes(16).toString('hex'),
+			MessageBody: JSON.stringify(
+				formatPaymentEventMessage(message)
+			)
+		}
+	}
+}
