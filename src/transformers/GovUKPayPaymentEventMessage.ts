809c5555f8a72afd0a9ba496beacd630eb270460
Steven <steven.fountain@digital.cabinet-office.gov.uk>
2020-04-22 17:13:33 +0100

    Fix `event_data` -> expected `event_details`

--- src/transformers/GovUKPayPaymentEventMessage.ts
+++ src/transformers/GovUKPayPaymentEventMessage.ts
@@ -5,7 +5,7 @@ import Transformer from './Transformer'
 import { Message } from './../providers/Provider'
 
 interface PaymentEventMessage {
-	event_data: { [key: string]: string };
+	event_details: { [key: string]: string };
 	resource_type?: string;
 	resource_external_id?: string;
 	event_date?: string;
@@ -21,10 +21,10 @@ function formatPaymentEventMessage(message: Message): PaymentEventMessage {
 		{ key: 'transaction_id', target: 'resource_external_id' },
 		{ key: 'parent_transaction_id', target: 'parent_resource_external_id' },
 		{ key: 'resource_type', target: 'resource_type' },
-		{ key: 'event_date', target: 'event_date' },
+		{ key: 'event_date', target: 'timestamp' },
 		{ key: 'event_type', target: 'event_type' }
 	]
-	const formatted: PaymentEventMessage = { event_data: {} }
+	const formatted: PaymentEventMessage = { event_details: {} }
 
 	// initially extract the reserved properties
 	for (const reserved of reservedKeys) {
@@ -38,7 +38,7 @@ function formatPaymentEventMessage(message: Message): PaymentEventMessage {
 	// any remaining properties will override attributes of the transaction itself
 	// put these in `event_data`
 	for (const paymentEventMessageKey in message) {
-		formatted.event_data[paymentEventMessageKey] = message[paymentEventMessageKey]
+		formatted.event_details[paymentEventMessageKey] = message[paymentEventMessageKey]
 	}
 	return formatted
 }
