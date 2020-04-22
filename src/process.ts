e145346c5bf2073cd645129e7751352323d3ac40
Steven <steven.fountain@digital.cabinet-office.gov.uk>
2020-04-22 16:19:19 +0100

    Use GovUKPayPaymentEventMessage transformer
    GOV.UK Pay will use a custom message transform for this use case, code
    kept up to date with fork.

--- src/process.ts
+++ src/process.ts
@@ -1,9 +1,10 @@
 import S3Provider from './providers/S3Provider'
 import SQSConsumer from './consumers/SQSConsumer'
+import GovUkPayPaymentEventMessage from './transformers/GovUKPayPaymentEventMessage'
 const { PROVIDER_S3_BUCKET_NAME, PROVIDER_S3_SOURCE_FILE, CONSUMER_SQS_QUEUE_URL } = process.env
 
 const s3Provider = new S3Provider(PROVIDER_S3_BUCKET_NAME, PROVIDER_S3_SOURCE_FILE)
-const sqs = new SQSConsumer(CONSUMER_SQS_QUEUE_URL).writer()
+const sqs = new SQSConsumer(CONSUMER_SQS_QUEUE_URL, new GovUkPayPaymentEventMessage()).writer()
 
 s3Provider
 	.stream()
