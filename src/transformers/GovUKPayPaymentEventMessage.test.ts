6214f8602cf4fb1d7ffb0347b6bd115b53ea99d7
Stephen Daly <stephen.daly@digital.cabinet-office.gov.uk>
2022-07-29 11:44:45 +0100

    PP-9873 Transform boolean values from string
    If a CSV column value has a value that is "true", or "false", transform
    this to a boolean type when creating the event to send to ledger.

--- src/transformers/GovUKPayPaymentEventMessage.test.ts
+++ src/transformers/GovUKPayPaymentEventMessage.test.ts
@@ -14,6 +14,11 @@ describe('message formatter', () => {
 		'will_have_empty_space': ' some-empty-space-values ',
 		'value_omitted': '',
 		'value_included': 'some-value',
+		'boolean_string_lowercase_true': 'true',
+		'boolean_string_uppercase_true': 'TRUE',
+		'boolean_string_lowercase_false': 'false',
+		'boolean_string_uppercase_false': 'FALSE',
+		'empty_column': ''
 	}
 
 	// @ts-ignore
@@ -38,6 +43,11 @@ describe('message formatter', () => {
 		expect(body).toHaveProperty('live', true)
 		expect(body).toHaveProperty('event_details.boolean_value', true)
 		expect(body).toHaveProperty('event_details.numeric_value', 123)
+		expect(body).toHaveProperty('event_details.boolean_string_lowercase_true', true)
+		expect(body).toHaveProperty('event_details.boolean_string_uppercase_true', true)
+		expect(body).toHaveProperty('event_details.boolean_string_lowercase_false', false)
+		expect(body).toHaveProperty('event_details.boolean_string_uppercase_false', false)
+		expect(body).not.toHaveProperty('empty_column')
 	})
 
 	test('ignores reserved properties if not needed on transaction', () => {
